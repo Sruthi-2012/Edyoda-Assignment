63806acd6ab69a1f56abb10438085e70cec1d64c
Sruthi-2012 <93936684+Sruthi-2012@users.noreply.github.com>
2022-01-08 21:06:27 +0530

    Add files via upload

---
+++ Questions_quiz.py
@@ -0,0 +1,24 @@
+quiz = {
+    1 : {
+        "question" : "Which among them is used to create an object ? \n a.class \n b. function \n c.Method\n d.constructor",
+        "answer":"d"},
+    2 : {
+        "question" : "Which among them is mutable? \n a.string,tuple \n b.dictionary,list \n c. set, list,dictionary \n d.all of the above",
+        "answer" : "a"},
+    3 : {
+        "question" : "Which is used to get decimal values? \n a.int \n b.float \n c.len \n d. dec",
+        "answer" : "b"
+    },
+    4 : {
+        "question" : "Who developed python language? \n a.Wick Van Rossum \n b.Rasmus Lerdoff \n c.Guido VanRossum \n d.Niene Storm",
+        "answer" : "c"
+    },
+    5 : {
+        "question" : "What is value of math.pow(2,5)? \n a. 10 \n b.25 \n c.32 \n d.12",
+        "answer" : "c"
+    },
+    6 : {
+        "question" : "correction extension of python file \n a. .py \n b. .python \n c. .pl \n d. p",
+        "answer" : "a"
+    }
+}
